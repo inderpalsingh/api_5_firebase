c8a2db9653068dfd382574c52c0e723fbfa642ca
Inderpal Singh <inderpalsmutta@gmail.com>
2024-06-01 19:47:44 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/firebaselocal/local/api_5_firebase/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.firebaselocal.local.api_5_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/splash_page/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:api_5_firebase/screen/home_screen/home_page.dart';
+import 'package:api_5_firebase/screen/login_screen/login_page.dart';
+import 'package:api_5_firebase/screen/signup_screen/signup_page.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserCheck();
+  }
+  
+  getUserCheck()async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? userId = prefs.getString(LoginPage.USER_ID);
+    print('User ID:  $userId');
+    if(userId !=null){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }else{
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignupPage()));
+    }
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
